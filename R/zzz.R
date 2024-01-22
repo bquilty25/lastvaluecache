8b26738228cf616200dde38b018090020b5fceea
Billy Quilty <bquilty25@gmail.com>
2024-01-22 17:34:28 +0000

    update .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onLoad <- function(libname = find.package("lastvaluecache"), pkgname = "lastvaluecache"){
-source("R/functions.R")
+
+  # Example functions to be run on load
+  addLastValueToList(42)       # Assuming this function has no side effects
+  setMaxCachedValues(3)        # Set a default for max cached values
+
+  # Consider printing a message to inform the user that functions are being run
+  message("Package LastValueCache loaded. Default max cached values set to 3.")
 }
