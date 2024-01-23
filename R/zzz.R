fa90f031eccc8064ee99335e2d2b3627ccf34e87
Billy Quilty <bquilty25@gmail.com>
2024-01-23 10:12:48 +0000

    update onload

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,13 @@
 .onLoad <- function(libname, pkgname) {
     # Ensure that 'last_values' and 'max_length' are initialized before the package is loaded.
     if(!exists("last_values", envir = .GlobalEnv)){
-      assign("last_values", list(), envir = .GlobalEnv)
+      assign("last_values", list(NULL,NULL,NULL), envir = .GlobalEnv)
     }
     if(!exists("max_length", envir = .GlobalEnv)){
       assign("max_length", 3, envir = .GlobalEnv)
     }
 
-    # Now that we have ensured 'last_values' and 'max_length' exist, we can safely use them.
-    # Here we might run a check or set them to a default state if needed.
+
 
     message("Package LastValueCache loaded with default settings.")
 }
