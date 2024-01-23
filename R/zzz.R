bf93b7ecc7ccc4abb45bb3499fd448b0fe8c1e5d
Billy Quilty <bquilty25@gmail.com>
2024-01-23 10:24:40 +0000

    update onload

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,19 @@
 .onLoad <- function(libname, pkgname) {
-    # Ensure that 'last_values' and 'max_length' are initialized before the package is loaded.
-    if(!exists("last_values", envir = .GlobalEnv)){
-      assign("last_values", list(NULL,NULL,NULL), envir = .GlobalEnv)
-    }
-    if(!exists("max_length", envir = .GlobalEnv)){
-      assign("max_length", 3, envir = .GlobalEnv)
-    }
-
-
-
-    message("Package LastValueCache loaded with default settings.")
+    # Ensure that 'last_values' and 'max_length' are initialized before the package is loaded
+  rlang::run_on_load()
 }
+
+on_load(
+  if(!exists("last_values", envir = .GlobalEnv)){
+    assign("last_values", list(NULL,NULL,NULL), envir = .GlobalEnv)
+  }
+  if(!exists("max_length", envir = .GlobalEnv)){
+    assign("max_length", 3, envir = .GlobalEnv)
+  }
+)
+
+on_load(setMaxCachedValues(max_length))
+
+on_load(NULL)
+on_load(NULL)
+on_load(NULL)
